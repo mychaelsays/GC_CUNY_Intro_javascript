81b5bd6ee673d7ffda15b4605b3a8d034bc00ffb
M <mlee8@gradcenter.cuny.edu>
2023-10-12 19:14:43 -0400

    10_12_Commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 alert('My project is going to be awesome!');
+$("#noShow").hide();
+$("h1").html("The Best Jokes Ever!");
+$("#joke").append("<p>Q. What do you call a fake noodle? <br /> A. An impasta</p>");
+$("#toggleJokes").click(function() {
+    $("#joke").toggle();
+});
+const response = prompt("Are these jokes good or bad?");
+if (response === "bad") {
+    $("h1").html("The Worst Jokes! :(");
+} else if (response === "good") {
+    $("h1").html("The Best Jokes Ever! :)");
+} else {
+    $("h1").html("The Best Jokes Ever!");
+}
